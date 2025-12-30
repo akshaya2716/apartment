8596a358e955746e5c9ff9ef0c8f10beb85b71a4
akshaya2716 <ssp.10011001@gmail.com>
2025-12-29 23:50:36 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminPage from "./AdminPage";
 import UserPage from "./UserPage";
+import AdminPage from "./AdminPage";
 
+/**
+ * App Root
+ * Handles all routing
+ */
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Home / User Page */}
+        <Route path="/" element={<UserPage />} />
+
+        {/* Admin Page */}
         <Route path="/admin" element={<AdminPage />} />
-        <Route path="/user" element={<UserPage />} />
+
+        {/* Fallback: redirect unknown routes to home */}
+        <Route path="*" element={<UserPage />} />
       </Routes>
     </BrowserRouter>
   );
